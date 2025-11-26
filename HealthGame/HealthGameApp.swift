3f502bbd0398d87d8174eef8cc85f19dc7e691e7
shivam-chaturvedi <chaturvedishivam598@gmail.com>
2025-11-26 10:24:53 +0530

    Initial Commit

---
+++ HealthGame/HealthGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  HealthGameApp.swift
+//  HealthGame
+//
+//  Created by Simran Kaur on 26/11/25.
+//
+
+import SwiftUI
+
+@main
+struct HealthGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
